39439ea4355628e153c9dc5536287ce6b1c93a81
rehannnnnn <hannn9491@gmail.com>
2021-12-20 00:32:07 +0700

    update register

--- Register/userRoute/register.js
+++ Register/userRoute/register.js
@@ -2,15 +2,19 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const userSchema = require("../schema/userSchema");
+const bcrypt = require("bcrypt");
+const salt = bcrypt.genSaltSync(10);
 
 router.post("/register", (req, res) => {
     const userModel = new userSchema();
+    const encryptedPassword = bcrypt.hashSync(req.body.password, salt);
     userModel.username = req.body.username;
-    userModel.password = req.body.password;
+    userModel.password = encryptedPassword;
     userModel.email = req.body.email;
     userModel.jenis_kelamin = req.body.jenis_kelamin;
     userModel.tanggal_lahir = req.body.tanggal_lahir;
     userModel.foto = req.body.foto;
+
 
     return userModel.save((err, data) => {
         if (err) {
